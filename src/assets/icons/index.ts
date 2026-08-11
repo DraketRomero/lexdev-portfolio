f0c0b9b21fd6590d9cbefb7b8215a7e01e061a2c
Diego Romero <draketromero@gmail.com>
2026-08-10 23:05:36 -0600

    Add: Icons to show on page

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,6 @@
+export { DownloadDocumentIcon } from './DownloadDocumentIcon';
+export { GithubIcon } from './GithubIcon';
 export { LanguageIcon } from './LanguageIcon';
+export { LinkedInIcon } from "./LinkedInIcon";
 export { MoonIcon } from "./MoonIcon";
 export { SunIcon } from "./SunIcon";
