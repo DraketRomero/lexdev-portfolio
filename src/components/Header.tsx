a8b23d25bf82ae23f37da060ea162c0b8a0d3822
Diego Romero <draketromero@gmail.com>
2026-08-10 23:11:48 -0600

    Add: Finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,12 @@ import './css/header.css';
 import img_dark from './../assets/img/logo-dark-theme.png';
 import img_light from './../assets/img/logo-light-theme.webp';
 import { LanguageIcon } from '../assets/icons/LanguageIcon';
+import { Link, useLocation } from 'react-router';
+import { Button } from "./Button";
 
 export const Header = () => {
     const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
-    const [isPressed, setIsPressed] = useState<boolean>(false);
+    const { pathname } = useLocation();
 
     const toggleTheme = () => {
         setIsDarkTheme(!isDarkTheme);
@@ -18,33 +20,56 @@ export const Header = () => {
             <div className="main-container">
                 <nav className="options-container">
                     <div className="img-container">
-                        <img src={isDarkTheme ? img_light : img_dark} alt="LexDev - Diego Romero" className="logo-img" />
+                        <Link to="/">
+                            <img src={isDarkTheme ? img_light : img_dark} alt="LexDev - Diego Romero" className="logo-img" />
+                        </Link>
                     </div>
 
                     <div>
                         <ul className="list-group">
-
-                            <li className={`menu-option ${isPressed ? 'menu-option-unselected' : 'menu-option-selected'}`}
-                                onMouseDown={() => setIsPressed(true)}
-                            >Proyectos</li>
-
-
-                            <li className={`menu-option ${isPressed ? 'menu-option-unselected' : 'menu-option-selected'}`}>Experiencia</li>
-                            <li className={`menu-option ${isPressed ? 'menu-option-unselected' : 'menu-option-selected'}`} onMouseDown={() => setIsPressed(true)}
-                                onMouseUp={() => setIsPressed(false)}
-                                onMouseLeave={() => setIsPressed(false)}>Sobre mi</li>
-                            <li className={`logo-theme`} onClick={toggleTheme}>
-                                <div className="logo-item-container">
-                                    <div className="logo-theme-container">{isDarkTheme ? MoonIcon : SunIcon}</div>
-                                </div>
+                            <li>
+                                <Button
+                                    className={`menu-option button-header`}
+                                    routeName="Proyectos"
+                                    routeDestination="/projects"
+                                    active={pathname == "/projects"}
+                                />
                             </li>
+
+
+                            <li>
+                                <Button
+                                    className={`menu-option button-header`}
+                                    routeName="Experiencia"
+                                    routeDestination="/experience"
+                                    active={pathname == "/experience"}
+                                />
+                            </li>
+
+                            <li>
+                                <Button
+                                    className={`menu-option button-header`}
+                                    routeName="Sobre mi"
+                                    routeDestination="/about-me"
+                                    active={pathname == "/about-me"}
+                                />
+                            </li>
+
+                            <li className="logo-theme" onClick={toggleTheme}>
+                                <button className="theme-toggle-button" onClick={toggleTheme}>
+                                    <div className="logo-item-container">
+                                        <div className="logo-theme-container">{isDarkTheme ? <MoonIcon /> : <SunIcon />}</div>
+                                    </div>
+                                </button>
+                            </li>
+
                             <li className={`logo-idioms`}>
                                 <div className="idioms-container">
                                     <div>
                                         <span className="icon-idioms">{LanguageIcon}</span>
                                         <select className="language-selector">
-                                            <option style={{ color: "white" }}>EN</option>
-                                            <option style={{ color: "white" }} >ES</option>
+                                            <option>EN</option>
+                                            <option>ES</option>
                                         </select>
                                     </div>
                                 </div>
