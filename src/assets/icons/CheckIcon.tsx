ae833802dbca1ca3c252111d86e05afa38a48cc5
Diego Romero <draketromero@gmail.com>
2026-08-19 23:42:29 -0600

    fix: Error properties on svg component

--- src/assets/icons/CheckIcon.tsx
+++ src/assets/icons/CheckIcon.tsx
@@ -1,5 +1,5 @@
 import type { IIconProps } from "./IIconProps";
 
 export const CheckIcon = ({ className }: IIconProps) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width="18px" height="18px" className={className}>
-  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>;
