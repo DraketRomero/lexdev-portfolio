2ba02edff872431e94821d5ab74a1d65c8c5c635
Diego Romero <draketromero@gmail.com>
2026-08-21 23:39:28 -0600

    add: i18n configuration to ContactComponent

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import './css/contactPage.css';
 import { useState } from 'react';
 
@@ -9,6 +10,7 @@ interface ContactData {
 
 
 export const ContactPage = () => {
+    const { t } = useTranslation();
     const [contactData, setContactData] = useState<ContactData>({ name: "", email: "", messsage: "" });
 
     const handleContactData = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, inputType: string) => {
@@ -32,54 +34,54 @@ export const ContactPage = () => {
     return (
         <div className='contact-contact-container'>
             <div className='titles-container'>
-                <h1 className='title'>¡Contáctame!</h1>
+                <h1 className='title'>{t('contact.titles.0')}</h1>
                 <br />
-                <em className='second-title'>¡Me encantará platicar contigo! :)</em>
+                <em className='second-title'>{t('contact.titles.1')}</em>
             </div>
             <div className='info-main-container'>
                 <div className='info-container'>
-                    <p className='info-title'>Correo</p>
+                    <p className='info-title'>{t('contact.contactInfo.mail')}</p>
                     <a href="" className='link-contact'>draketromero@gmail.com</a>
                 </div>
                 <div className='info-container'>
-                    <p className='info-title'>Telefono</p>
+                    <p className='info-title'>{t('contact.contactInfo.phone')}</p>
                     <a href="" className='link-contact'>+52 22 226 13 45 31</a>
                 </div>
                 <div className='info-container'>
-                    <p className='info-title'>Ubicacion</p>
+                    <p className='info-title'>{t('contact.contactInfo.location')}</p>
                     <span className='work-place'>Puebla, Mx</span>
                 </div>
                 <div className='info-container last'>
-                    <p className='info-title'>Disponibilidad</p>
-                    <p className='work-place'>Con amplia disponibilidad para oportunidades tanto hibiridas como remotas.</p>
+                    <p className='info-title'>{t('contact.contactInfo.availability')}</p>
+                    <p className='work-place'>{t('contact.contactInfo.availabilityMessage')}</p>
                 </div>
             </div>
             <div className='form-main-container'>
                 <form onSubmit={handleSendContactInfo} autoComplete='off' method='POST' id='contact-form'>
                     <div className='form-field-container'>
                         <label htmlFor="name" className='input-label'>
-                            <span className='form-label'>Nombre</span>
-                            <input type="text" id="name" placeholder='John Doe' className='input-text' value={contactData.name} onChange={(e) => handleContactData(e, "name")} autoComplete='off' />
+                            <span className='form-label'>{t('contact.contactForm.name.label')}</span>
+                            <input type="text" id="name" placeholder={`${t('contact.contactForm.name.placeholder')}`} className='input-text' value={contactData.name} onChange={(e) => handleContactData(e, "name")} autoComplete='off' />
                         </label>
                     </div>
                     <div className='form-field-container'>
                         <label htmlFor="email" className='input-label'>
-                            <span className='form-label'>Correo</span>
-                            <input type="email" id="email" placeholder='jhondoe@mail.com' className='input-text' value={contactData.email} onChange={(e) => handleContactData(e, "email")} autoComplete='off' />
+                            <span className='form-label'>{t('contact.contactForm.email.label')}</span>
+                            <input type="email" id="email" placeholder={`${t('contact.contactForm.email.placeholder')}`} className='input-text' value={contactData.email} onChange={(e) => handleContactData(e, "email")} autoComplete='off' />
                         </label>
 
                     </div>
                     <div className='form-field-container'>
                         <label htmlFor="message" className='input-label'>
-                            <span className='form-label'>Mensaje</span>
-                            <textarea id="message" placeholder="Tu mensaje :)" className='text-area' value={contactData.messsage} onChange={(e) => handleContactData(e, "message")} />
+                            <span className='form-label'>{t('contact.contactForm.message.label')}</span>
+                            <textarea id="message" placeholder={`${t('contact.contactForm.message.placeholder')}`} className='text-area' value={contactData.messsage} onChange={(e) => handleContactData(e, "message")} />
                         </label>
                     </div>
                     <div className='message-button-container'>
-                        <p className='message'>Me pondre en contacto contigo lo mas pronto posible.</p>
+                        <p className='message'>{t('contact.contactForm.textBottom')}</p>
 
                         <div className='button-contact-contaner'>
-                            <button type='submit' className='button-form'>Enviar</button>
+                            <button type='submit' className='button-form'>{t('contact.contactForm.textButtonBottom')}</button>
                         </div>
                     </div>
                 </form>
