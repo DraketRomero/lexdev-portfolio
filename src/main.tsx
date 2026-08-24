61dbde21f4da229c88698a0a6a94c03272ff7b12
Diego Romero <draketromero@gmail.com>
2026-08-24 16:37:11 -0600

    upd: Refactor root component to implement Context API

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import { routes } from './routes/routes.tsx';
+import { createRoot } from "react-dom/client";
+import { MainProvider } from "./MainProvider";
 import './i18n/config';
-
-const router = createBrowserRouter(routes)
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={router} />
+    <MainProvider />
 )
