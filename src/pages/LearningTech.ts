6f3aac768b205c92f5a192c35976c98efcc66df4
Diego Romero <draketromero@gmail.com>
2026-08-26 15:48:59 -0600

    add: Learning array section category with technology icons

---
+++ src/pages/LearningTech.ts
@@ -0,0 +1,11 @@
+import { FastApiIcon } from "../assets/icons/FastApiIcon";
+import type { ITechComponent } from "../components/interfaces/ITechComponent";
+
+export const LearningIcons: ITechComponent[] = [
+    {
+        section: "",
+        tools: [
+            { name: "FastApi", Icon: FastApiIcon },
+        ]
+    },
+];
