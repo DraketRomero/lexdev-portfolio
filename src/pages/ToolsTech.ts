8eba2df71ea28c30c6cebcd8aa4484da5eb8c05a
Diego Romero <draketromero@gmail.com>
2026-08-26 15:48:48 -0600

    add: Tools array section category with technology icons

---
+++ src/pages/ToolsTech.ts
@@ -0,0 +1,33 @@
+import { 
+    GithubIcon, 
+    GitIcon, 
+    IntelliJIDEAIcon, 
+    PostmanIcon, 
+    VSCodeIcon, 
+    ZedIcon 
+} from '../assets/icons';
+import type { ITechComponent } from '../components/interfaces/ITechComponent';
+
+export const toolsIcons: ITechComponent[] = [
+    {
+        section: "IDE'S",
+        tools: [
+            { name: "VsCode", Icon: VSCodeIcon },
+            { name: "Zed", Icon: ZedIcon },
+            { name: "IntelliJ IDEA", Icon: IntelliJIDEAIcon },
+        ]
+    },
+    {
+        section: "Versionamiento",
+        tools: [
+            { name: "Git", Icon: GitIcon },
+            { name: "GitHub", Icon: GithubIcon },
+        ]
+    },
+    {
+        section: "Consumo de REST API's",
+        tools: [
+            { name: "Postman", Icon: PostmanIcon },
+        ]
+    },
+];
