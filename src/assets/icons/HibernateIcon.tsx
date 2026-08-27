22d0616ea301a1fb0fd0e20195bcaabfe1d7fe96
Diego Romero <draketromero@gmail.com>
2026-08-26 20:02:43 -0600

    add: new icons

---
+++ src/assets/icons/HibernateIcon.tsx
@@ -0,0 +1,5 @@
+import type { IIconProps } from "./IIconProps";
+
+export const HibernateIcon = ({ width = 24, height = 24 }: IIconProps) => <svg viewBox="0 0 128 128" fill="currentColor" width={width} height={height}>
+        <path fill="curentColor" d="m29.246 3.766 23.168 40.129-23.18 40.19-23.156-40.19Zm69.508 120.468L75.586 84.105l23.18-40.19 23.156 40.19ZM75.594 3.766H29.258L52.43 43.898h46.35ZM52.406 124.23H98.75L75.594 84.102H29.219Zm0 0"></path>
+    </svg>
