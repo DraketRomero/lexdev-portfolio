cd043f801ec395a30f6b5f19d8989a9427b2be4f
Diego Romero <draketromero@gmail.com>
2026-09-01 21:52:55 -0600

    add: Component to generate the paws way through every job experience

---
+++ src/components/DogPawsWayLR.tsx
@@ -0,0 +1,134 @@
+import { DogFilledIcon, DogOutlinedIcon } from "../assets/icons"
+
+export const DogPawsWayLR = () => {
+    return (
+        <div style={{ position: "relative", height: "300px", width: "100%", margin: "30px 0 50px 0", padding: "0 0 20px 0", color: "var(--secondary-color)" }}>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "0%", left: "90%", transform: "rotate(0deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "10%", left: "80%", transform: "rotate(0deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "22%", left: "88%", transform: "rotate(20deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "28%", left: "75%", transform: "rotate(20deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "40%", left: "80%", transform: "rotate(50deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "43%", left: "68%", transform: "rotate(50deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "55%", left: "70%", transform: "rotate(70deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "50%", left: "55%", transform: "rotate(70deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "62%", left: "50%", transform: "rotate(60deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "55%", left: "38%", transform: "rotate(60deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "68%", left: "35%", transform: "rotate(50deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "65%", left: "20%", transform: "rotate(50deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "78%", left: "20%", transform: "rotate(35deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "82%", left: "5%", transform: "rotate(35deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "94%", left: "15%", transform: "rotate(15deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "100%", left: "3%", transform: "rotate(15deg)" }}>
+                <DogFilledIcon width={24} height={24} />
+            </div>
+
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "110%", left: "15%", transform: "rotate(0deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+            </div>
+        </div>
+    )
+}
+
+// import { DogFilledIcon, DogOutlinedIcon } from "../assets/icons"
+
+// export const DogPawsWayLR = () => {
+//     return (
+
+//         <div style={{ position: "relative", height: "300px", width: "100%", margin: "30px 0 50px 0", padding: "0 0 20px 0", color: "var(--secondary-color)" }}>
+//             izquierda - derecha
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "0%", left: "90%", transform: "rotate(0deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "10%", left: "80%", transform: "rotate(20deg)" }}>
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "25%", left: "80%", transform: "rotate(40deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "20%", left: "60%", transform: "rotate(0deg)" }}>
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "22%", left: "85%", transform: "rotate(20deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "28%", left: "75%", transform: "rotate(20deg)" }}>
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+
+            
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "40%", left: "80%", transform: "rotate(30deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "42%", left: "68%", transform: "rotate(30deg)" }}>
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "55%", left: "75%", transform: "rotate(40deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "58%", left: "60%", transform: "rotate(40deg)" }}>
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+
+            
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "65%", left: "50%", transform: "rotate(60deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "65%", left: "50%", transform: "rotate(60deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "65%", left: "30%", transform: "rotate(50deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "80%", left: "12%", transform: "rotate(35deg)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+//             <div style={{ position: "absolute", height: "24px", width: "24px", top: "100%", left: "0%", transform: "rotate(0)" }}>
+//                 <DogOutlinedIcon width={24} height={24} />
+//                 <DogFilledIcon width={24} height={24} />
+//             </div>
+//         </div>
+//     )
+// }
---
+++ src/components/DogPawsWayRL.tsx
@@ -0,0 +1,37 @@
+import { DogFilledIcon, DogOutlinedIcon } from "../assets/icons"
+
+export const DogPawsWayRL = () => {
+    return (
+        <div style={{ position: "relative", height: "300px", width: "100%", margin: "30px 0 50px 0", padding: "0 0 20px 0" }}>
+            derecha -izquierda
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "0%", left: "5%", transform: "rotate(-8deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+                <DogFilledIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "22%", left: "10%", transform: "rotate(-40deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+                <DogFilledIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "40%", left: "25%", transform: "rotate(-50deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+                <DogFilledIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "55%", left: "45%", transform: "rotate(-60deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+                <DogFilledIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "65%", left: "65%", transform: "rotate(-50deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+                <DogFilledIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "80%", left: "80%", transform: "rotate(-20deg)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+                <DogFilledIcon width={24} height={24} />
+            </div>
+            <div style={{ position: "absolute", height: "24px", width: "24px", top: "100%", left: "85%", transform: "rotate(0)" }}>
+                <DogOutlinedIcon width={24} height={24} />
+                <DogFilledIcon width={24} height={24} />
+            </div>
+        </div>
+    )
+}
