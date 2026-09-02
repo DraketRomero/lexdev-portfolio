d436aefb87c3f9e563cfc699c995bbcc1c2b65da
Diego Romero <draketromero@gmail.com>
2026-09-01 21:54:11 -0600

    upd: Market for experience page

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -1,3 +1,18 @@
+import { JobExperienceCard } from "../components/JobExperienceCard"
+import { jobs } from "./data/JobExperience"
+
 export const ExperiencePage = () => {
-    return <>Experience Page</>
+    return (
+        <div style={{ width: "100%" }}>
+            <div style={{ padding: "20px 40px 0 40px" }}>
+                <div style={{ padding: "0 0 15px 0" }}>
+                    <p style={{ fontSize: "25px", fontStyle: "italic", fontWeight: "bolder", fontFamily: "'Fraunces'", color: "var(--text-color)" }}>Experience</p>
+                </div>
+
+                {
+                    jobs.map((exp, i) => <JobExperienceCard key={i} id={i}  experience={exp} length={jobs.length} />)
+                }
+            </div>
+        </div>
+    )
 }
